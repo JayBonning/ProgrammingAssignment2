2ecfbe5ea4bc8662aa20fdba984728b8caccbf5d
Jay Bonning <fbonning@gmail.com>
2015-09-25 15:51:59 -0400

    Created the makeCacheMatric object and methods

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## Makes a object call makeCacheMatrix, which takes argument x as the base type of the matrix
+makeCacheMatrix <- function(x = numeric()) {
+  
+  #Internal cache
+  cacheMatrix <- NULL
+  
+  #Set Function clears out the current value and sets the new matric type passed in
+  set <- function(y) {
+    x <<- y
+    CacheMatrix <<- NULL
+  }
+  
+  #Gets the information that this was started with
+  get <- function() x
+  
+  #Takes the input and uses the solve command to calculate the inverse.  This is then cached 
+  setinverse <- function(inMatrix) cacheMatrix <<- solve(inMatrix)
+  
+  #Gets the previously cache matrix
+  getinverse <- function() cacheMatrix
+  
+  
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
